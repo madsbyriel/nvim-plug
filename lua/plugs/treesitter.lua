27f958b3651869efe9720ae801dbe25cebfa8f01
Mads Hvid Byriel <mhb@norlysenergytrading.com>
2026-08-05 14:30:29 +0200

    treesitter stuff

--- lua/plugs/treesitter.lua
+++ lua/plugs/treesitter.lua
@@ -1,18 +1,15 @@
--- require'nvim-treesitter.configs'.setup {
---     ensure_installed = { "lua", "rust", "zig", "python", "c_sharp", "go" }, -- Auto-install C# parser
---     auto_install = true,
---     highlight = {
---         enable = true,
--- 
---         -- default highlighting is better than these
---         disable = {
---             "csv",
---             "gitcommit"
---         }
---     },
--- }
--- 
--- vim.api.nvim_create_autocmd('FileType', {
---     pattern = { 'cs' },
---     callback = function() vim.treesitter.start() end,
--- })
+require('nvim-treesitter').install({
+    'lua', 'vim', 'vimdoc', 'bash',
+    'rust', 'zig', 'python', 'c_sharp', 'go',
+    'markdown', 'markdown_inline', 'json', 'yaml', 'toml',
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    callback = function(args)
+        local ft = vim.bo[args.buf].filetype
+        local lang = vim.treesitter.language.get_lang(ft) or ft
+        if vim.treesitter.language.add(lang) then
+            vim.treesitter.start(args.buf, lang)
+        end
+    end,
+})
